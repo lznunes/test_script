9ff5b150564fa89970dfbcf80928a521e480f987
Luiz Nunes <lznunes@gmail.com>
2023-11-19 18:05:41 -0300

    incluso teste firefox e screenshot

--- src/test/java/com/topcoder/TestCase.java
+++ src/test/java/com/topcoder/TestCase.java
@@ -1,37 +1,79 @@
 package com.topcoder;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 public class TestCase {
 
-    private WebDriver driver;
+    private WebDriver driverCh;
+    private WebDriver driverFx;
 
     @BeforeMethod
     public void setUp() {
         // Configuração automática do WebDriver usando o WebDriverManager
-        WebDriverManager.chromedriver().clearResolutionCache();
         WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
 
-        // Inicializar o WebDriver
-        driver = new ChromeDriver();
+        // Inicializar os WebDrivers
+        driverCh = new ChromeDriver();
+        driverFx = new FirefoxDriver();
     }
 
     @Test
-    public void myTest() {
+    public void myTestChrome() {
+        runTest(driverCh);
+    }
+
+    @Test
+    public void myTestFirefox() {
+        runTest(driverFx);
+    }
+
+    private void runTest(WebDriver driver) {
         driver.get("https://www.google.com");
         // Adicione a lógica do teste aqui
     }
 
     @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
+    public void tearDown(ITestResult result) {
+        // Captura de screenshot apenas em caso de falha
+        if (result.getStatus() == ITestResult.SUCCESS) {
+            captureScreenshot(driverCh, "Chrome");
+            captureScreenshot(driverFx, "Firefox");
+        }
+
+        if (driverCh != null) {
+            driverCh.quit();
+        }
+
+        if (driverFx != null) {
+            driverFx.quit();
+        }
+    }
+
+    private void captureScreenshot(WebDriver driver, String browserName) {
+        try {
+            if (driver != null) {
+                TakesScreenshot ts = (TakesScreenshot) driver;
+                byte[] screenshotBytes = ts.getScreenshotAs(OutputType.BYTES);
+
+                // Salvar a screenshot em um local desejado
+                // Por exemplo, você pode usar uma biblioteca como Apache Commons IO para salvar o arquivo
+                FileUtils.writeByteArrayToFile(new File("src/main/resources/screenshot_" + browserName + ".png"), screenshotBytes);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
